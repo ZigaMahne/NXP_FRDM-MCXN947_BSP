d642738af54cf523aa080c99375464a3a019beb8
Vladimir Umek <vladimir.umek@arm.com>
2025-03-04 12:42:21 +0100

    Templates: Add memory regions to ROM/RAM device template

---
+++ boards/frdmmcxn947/cmsis/templates/Device/Simple2/regions_rom.h
@@ -0,0 +1,37 @@
+#ifndef REGIONS_ROM_H
+#define REGIONS_ROM_H
+
+//-------- <<< Use Configuration Wizard in Context Menu >>> --------------------
+//------ With VS Code: Open Preview for Configuration Wizard -------------------
+
+// <i> Device Family Pack (DFP):   NXP::MCXN947_DFP@19.0.0
+
+// <h> Core 0 Memory Configuration
+// ===============================
+//   <o> ROM Base Address <0x0-0xFFFFFFFF:8>
+//   <i> Defines base address of memory region. Default: 0x00000000
+//   <i> Contains Startup and Vector Table
+#define __ROM0_BASE         0x00000000
+//   <o> ROM Region Size [bytes] <0x0-0xFFFFFFFF:8>
+//   <i> Defines size of memory region. Default: 0x00000000
+#define __ROM0_SIZE         0x000C0000
+
+//   <o> RAM Base Address <0x0-0xFFFFFFFF:8>
+//   <i> Defines base address of memory region. Default: 0x00000000
+//   <i> Contains uninitialized RAM, Stack, and Heap
+#define __RAM0_BASE         0x20000000
+//   <o> RAM Region Size [bytes] <0x0-0xFFFFFFFF:8>
+//   <i> Defines size of memory region. Default: 0x00000000
+#define __RAM0_SIZE         0x0004E000
+
+//   <o> Stack Size (in Bytes) <0x0-0xFFFFFFFF:8>
+#define __STACK_SIZE        0x00000400
+
+//   <o> Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
+#define __HEAP_SIZE         0x00000C00
+// </h>
+// </h>
+
+//-------- End of configuration section --------
+
+#endif /* REGIONS_ROM_H */
